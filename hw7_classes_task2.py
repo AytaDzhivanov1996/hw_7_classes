8b6c2f96fedb17dae63b888b8f82499adedb18e2
AytaDzhivanov1996 <kurt.alive.1994@gmail.com>
2022-12-16 11:08:04 +0300

    исправления

--- hw7_classes_task2.py
+++ hw7_classes_task2.py
@@ -6,15 +6,13 @@ def read_file_timed(file):
     start_time = time.time()
     try:
         f = open(file, 'rb')
-        print(f'Time required for {file} = {start_time}')
     except FileNotFoundError as e:
         print(e)
         print(f'Time required for {file} = 0.0')
     else:
         print(f.read())
         f.close()
+        print(f'Time required for {file} = {start_time}')
         return f
     finally:
-        print(time.time())
-
-
+        print('Finishing...')
